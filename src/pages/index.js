852ccebf07d5d6bb851eadef688fea9befbd1ec0
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-06-21 18:27:39 +0530

    Routing

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import AboutPage from "./AboutPage";
+import ContactPage from './ContactPage'
+import ResumePage from './ResumePage'
+import ProjectPage from './ProjectPage'
+
+export {AboutPage,ResumePage,ContactPage,ProjectPage}
