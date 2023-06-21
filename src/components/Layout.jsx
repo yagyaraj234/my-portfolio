fb497fbac87ec6261f6c9889a32d9316be8051e2
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-06-21 18:26:15 +0530

    Header added in layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import LeftBar from "./LeftBar";
+import RightBar from "./RightBar";
 const Layout = () => {
   return (
     <div className="bg-smoky-black flex md:flex-row flex-col  md:gap-8 gap-5 md:px-[30px] md:py-[60px] px-2 py-4 ">
@@ -7,7 +8,7 @@ const Layout = () => {
         <LeftBar />
       </div>
       <div className="w-full bg-eerie-black-1 rounded-[20px] box-shadow">
-        Right
+        <RightBar/>
       </div>
     </div>
   );
