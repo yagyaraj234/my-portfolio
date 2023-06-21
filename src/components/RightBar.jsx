6ab690b0a2f2460555b08ad129a6b965b3460bf1
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-06-21 18:27:21 +0530

    right section has header with sctive classes

---
+++ src/components/RightBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+const RightBar = () => {
+  return (
+    <div className="p-3 px-8 relative ">
+      <NavBar />
+      
+    </div>
+  );
+};
+
+export default RightBar;
