b7a339c6f4a65b42d80fa48d37e281141604f788
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-06-22 01:53:20 +0530

    nav section changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -20,7 +20,7 @@ const NavBar = () => {
       </Routes>
 
       <div className="absolute top-0 border-gray-600  right-0 px-10 border-b rounded-bl-3xl rounded-tr-[22px] border-l z-10 bg-onyx ">
-        <ul className="flex gap-20 py-5 font-sans font-semibold">
+        <ul className="flex gap-10 py-5 font-sans font-semibold">
           <NavLink
             className=" hover:text-light-gray-70 cursor-pointer transition-colors  duration-300 text-light-gray "
             to="/"
