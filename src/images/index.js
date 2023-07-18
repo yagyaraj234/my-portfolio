97a46f004fa75dc48aec9e2dab8b47e9fadc6ca0
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-07-18 21:04:38 +0530

    new images added

---
+++ src/images/index.js
@@ -0,0 +1,14 @@
+import html from './html.png'
+import css from './css.png'
+import git from './git.png'
+import cpp from './c++.png'
+import sql from './sql.png'
+import JS from './JS.png'
+import c from './c.png'
+import mongoDB from './mongoDB.png'
+import reactimg from './react.png'
+import reduximg from './redux.png'
+
+export{
+    html,css,git,cpp,sql,c,JS,reactimg,mongoDB,reduximg
+}
