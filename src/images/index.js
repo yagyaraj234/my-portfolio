054fc33a48aa0a8325c38c6861dafb101b833c4b
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-07-19 23:07:48 +0530

    new image added

--- src/images/index.js
+++ src/images/index.js
@@ -1,14 +1,25 @@
-import html from './html.png'
-import css from './css.png'
-import git from './git.png'
-import cpp from './c++.png'
-import sql from './sql.png'
-import JS from './JS.png'
-import c from './c.png'
-import mongoDB from './mongoDB.png'
-import reactimg from './react.png'
-import reduximg from './redux.png'
+import html from "./html.png";
+import css from "./css.png";
+import git from "./git.png";
+import cpp from "./c++.png";
+import sql from "./sql.png";
+import JS from "./JS.png";
+import c from "./c.png";
+import mongoDB from "./mongoDB.png";
+import reactimg from "./react.png";
+import reduximg from "./redux.png";
+import iconDev from "./icon-photo.svg";
 
-export{
-    html,css,git,cpp,sql,c,JS,reactimg,mongoDB,reduximg
-}
+export {
+  html,
+  css,
+  git,
+  cpp,
+  sql,
+  c,
+  JS,
+  reactimg,
+  mongoDB,
+  reduximg,
+  iconDev,
+};
