55d6138a401f559e4f87a0eb956c08d6474addb6
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-07-20 02:01:40 +0530

    responsive

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,10 +4,10 @@ import RightBar from "./RightBar";
 const Layout = () => {
   return (
     <div className="bg-smoky-black flex md:flex-row flex-col  md:gap-8 gap-5 md:px-[30px] md:py-[60px] px-2 py-4  ">
-      <div className="md:w-[350px] bg-eerie-black-2 rounded-[20px]  md:py-10  md:pl-0 box-shadow box-border min-h-[850px] max-h-[1200px] ">
+      <div className="md:w-[30%] bg-eerie-black-2 rounded-[20px]  md:py-10  md:pl-0 box-shadow box-border min-h-[850px] max-h-[800px]  sticky top-0">
         <LeftBar />
       </div>
-      <div className="w-full bg-eerie-black-1 rounded-[20px] box-shadow min-h-[850px]  navscr">
+      <div className="w-full bg-eerie-black-1 rounded-[20px] box-shadow min-h-[850px]  navscr max-w-[70%]">
         <RightBar/>
       </div>
     </div>
