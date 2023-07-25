fc608353f35f5f82306afa20cc30c3ca5a83b9cd
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-07-25 23:40:52 +0530

    layout updated

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,7 @@ const Layout = () => {
   return (
     <div className="bg-smoky-black flex md:flex-row flex-col  md:gap-8 gap-5 lg:px-[80px] 2xl:px-[150px] 
     xl:px-[120px] md:px-[40px] md:py-[40px] px-2 py-2">
-      <div className="md:w-[30%] bg-eerie-black-2 rounded-[20px]  md:py-10  md:pl-0 box-shadow box-border md:min-h-[850px] md:max-h-[800px]  sticky top-0 z-20">
+      <div className="md:w-[30%] bg-eerie-black-2 rounded-[20px]  md:py-10  md:pl-0 box-shadow box-border md:min-h-[800px] md:max-h-[800px]  sticky top-0 z-20">
         <LeftBar />
       </div>
       <div className="w-full bg-eerie-black-1 rounded-[20px] box-shadow md:min-h-[850px] min-h-[750px]  navscr md:max-w-[70%] pb-8">
