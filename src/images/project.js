54926c95b5973720f0809cc992a0dd4c729cb34c
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-09-05 14:48:04 +0530

    project page

--- src/images/project.js
+++ src/images/project.js
@@ -2,5 +2,6 @@ import project1 from "./project-1.png";
 import project2 from "./project-2.png";
 import project3 from "./project-3.png";
 import project4 from "./project-4.png";
+import project5 from "./project-5.png";
 
-export { project1, project2, project3, project4 };
+export { project1, project2, project3, project4 ,project5};
